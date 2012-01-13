ff95d6be54a7a23ff338f9d447358ea9821a182a
Rustam Aliyev <rustam@code.az>
2012-01-13 01:08:31 +0000

    add license block

--- modules/core/src/main/java/com/elasticinbox/core/account/validator/IValidator.java
+++ modules/core/src/main/java/com/elasticinbox/core/account/validator/IValidator.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2011 Optimax Software Ltd
+ * 
+ * This file is part of ElasticInbox.
+ * 
+ * ElasticInbox is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * ElasticInbox is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * ElasticInbox. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.elasticinbox.core.account.validator;
 
 import com.elasticinbox.core.model.Mailbox;
