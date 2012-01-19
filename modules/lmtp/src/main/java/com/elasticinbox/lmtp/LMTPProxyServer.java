41f024b83d330c877dce276f748ca8c6a8fc32e0
Rustam Aliyev <rustam@code.az>
2012-01-19 17:45:54 +0000

    Update to james-protocols 1.6.0

--- modules/lmtp/src/main/java/com/elasticinbox/lmtp/LMTPProxyServer.java
+++ modules/lmtp/src/main/java/com/elasticinbox/lmtp/LMTPProxyServer.java
@@ -24,6 +24,7 @@ import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.james.protocols.api.logger.Logger;
 import org.apache.james.protocols.lmtp.LMTPProtocolHandlerChain;
 import org.apache.james.protocols.netty.NettyServer;
 import org.apache.james.protocols.smtp.MailEnvelope;
@@ -35,6 +36,7 @@ import com.elasticinbox.lmtp.delivery.IDeliveryAgent;
 import com.elasticinbox.lmtp.server.LMTPServerConfig;
 import com.elasticinbox.lmtp.server.api.DeliveryReturnCode;
 import com.elasticinbox.lmtp.server.api.handler.ElasticInboxDeliveryHandler;
+import com.elasticinbox.lmtp.utils.JamesProtocolsLogger;
 
 /**
  * LMTP proxy main class which sends traffic to multiple registered handlers
@@ -52,10 +54,13 @@ public class LMTPProxyServer
 
 	public void start() throws Exception
 	{
+		Logger logger = new JamesProtocolsLogger();
+
 		LMTPProtocolHandlerChain chain = new LMTPProtocolHandlerChain();
 		chain.add(0, new ElasticInboxDeliveryHandler(backend));
 		chain.wireExtensibleHandlers();
-		server = new NettyServer(new SMTPProtocol(chain, new LMTPServerConfig()));
+
+		server = new NettyServer(new SMTPProtocol(chain, new LMTPServerConfig(), logger));
 		server.setListenAddresses(new InetSocketAddress(Configurator.getLmtpPort()));
 		server.setMaxConcurrentConnections(Configurator.getLmtpMaxConnections());
 		server.setTimeout(LMTPServerConfig.CONNECTION_TIMEOUT);
